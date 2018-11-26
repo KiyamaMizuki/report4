2871b4f73d3d0e9cea3457addbd94f3d6e480668
KiyamaMizuki <e185758@ie.u-ryukyu.ac.jp>
2018-11-26 15:27:20 +0900

    step2

--- src/Report4.java
+++ src/Report4.java
@@ -1,11 +1,13 @@
 public class Report4 extends Exception{
     public static void main(String[] args) {
-        String str = null;
+        /*String str = null;
         try {
             System.out.println(str.length());
         } catch (Exception e) {
             System.out.println("NullPointerException");
             System.out.println(e.getMessage());
-        }
+        }*/
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
